77b467fe0a0781c0c4f5b66b91316c3c1c2ecd8c
kva <vasickru@mail.ru>
2019-03-20 21:10:18 +0500

    creat files for tests

--- users_data_util.php
+++ users_data_util.php
@@ -5,3 +5,26 @@
  * Date: 20.03.2019
  * Time: 19:49
  */
+
+//var_dump($argv);
+
+$delimiter = ';'; // default delimiter for csv file
+
+foreach ($argv as $item) {
+    switch ($item) {
+        case 'comma':
+            $delimiter = ',';
+            break;
+        case 'semicolon':
+            $delimiter = ';';
+            break;
+        case 'countAverageLineCount':
+            //countAverageLineCount($delimiter);
+            break;
+        case 'replaceDates':
+            //replaceDates($delimiter);
+            break;
+    }
+}
+
+
